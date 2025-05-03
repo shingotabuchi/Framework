c96d245b7a6b8c174f975a03be1de0d35f83a0c8
Shingo Tabuchi <tabuchishingo123@gmail.com>
2025-05-03 13:56:30 +0900

    fix

--- Packages/com.shingotabuchi.framework/Runtime/Utility/MathUtils.cs
+++ Packages/com.shingotabuchi.framework/Runtime/Utility/MathUtils.cs
@@ -3,11 +3,11 @@ using System.Runtime.CompilerServices;
 public static class MathUtils
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static float Square(float x) => x * x;
+    public static float Square(this float x) => x * x;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static double Square(double x) => x * x;
+    public static double Square(this double x) => x * x;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int Square(int x) => x * x;
+    public static int Square(this int x) => x * x;
 }
