b614e33884a035635e20b7f676485e8afda9b168
Shingo Tabuchi <tabuchishingo123@gmail.com>
2025-05-16 22:31:08 +0900

    no message

--- Packages/com.shingotabuchi.framework/Runtime/UI/View.cs
+++ Packages/com.shingotabuchi.framework/Runtime/UI/View.cs
@@ -1,17 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Fwk.UI
 {
     public class View : MonoBehaviour
     {
+        private List<View> _childViews = new List<View>();
+
+        protected virtual void Awake()
+        {
+            GetChildViews();
+        }
+
+        protected void GetChildViews()
+        {
+            _childViews.Clear();
+            _childViews.AddRange(GetComponentsInChildren<View>(true));
+        }
+
         public virtual void Show()
         {
             gameObject.SetActiveFast(true);
+            foreach (var childView in _childViews)
+            {
+                childView.Show();
+            }
         }
 
         public virtual void Hide()
         {
             gameObject.SetActiveFast(false);
+            foreach (var childView in _childViews)
+            {
+                childView.Hide();
+            }
+        }
+
+        public virtual void OnParentShow()
+        {
+        }
+
+        public virtual void OnParentHide()
+        {
         }
     }
 }
