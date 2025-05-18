f7f56160593f79e01cd85384c61da224445839a3
Shingo Tabuchi <tabuchishingo123@gmail.com>
2025-05-18 10:08:44 +0900

    render feature

--- Packages/com.shingotabuchi.framework/Runtime/Rendering/UIRenderFeature.cs
+++ Packages/com.shingotabuchi.framework/Runtime/Rendering/UIRenderFeature.cs
@@ -1,31 +1,35 @@
+// ref. https://media.colorfulpalette.co.jp/n/nffc0ece136be
+
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class UIRendererFeature : ScriptableRendererFeature
 {
-    [SerializeField]
-    private LayerMask _layerMask;
+    [SerializeField] private LayerMask _layerMask;
 
-    [SerializeField]
-    private bool _execblur = false;
-    [SerializeField]
-    private bool _execGlassMorphism = false;
+#if UNITY_EDITOR
+    [SerializeField] private bool _debugForceChange;
+    [SerializeField] private bool _debugExecBlur;
+    [SerializeField] private bool _debugExecGlassMorphism;
+    [SerializeField][Range(0.0f, 1.0f)] private float _debugBlurRate = 1.0f;
+#endif
 
-    [SerializeField]
-    [Range(0.0f, 1.0f)]
-    private float _blurRate = 1.0f;
-    [SerializeField]
-    [Range(0.5f, 3.0f)]
-    private float _blurWidth = 2.0f;
-    [SerializeField]
-    [Range(0.1f, 1.0f)]
-    private float _blurRenderScale = 0.5f;
+    [SerializeField][Range(0.5f, 3.0f)] private float _blurWidth = 1.2f;
+    [SerializeField][Range(0.1f, 1.0f)] private float _blurRenderScale = 0.5f;
 
-    [SerializeField]
-    private Shader _blurShader;
+    [SerializeField] private Shader _blurShader;
 
     private UIRenderPass _uiRenderPass;
+
+    private readonly UIRendererFeatureParameter _parameter = new();
+
+    public static UIRendererFeature Instance { get; private set; }
+
+    public static bool ExistsInstance()
+    {
+        return Instance != null;
+    }
 
     public override void Create()
     {
@@ -34,8 +38,9 @@ public class UIRendererFeature : ScriptableRendererFeature
             return;
         }
 
-        Material blurMaterial = CoreUtils.CreateEngineMaterial(_blurShader);
+        var blurMaterial = CoreUtils.CreateEngineMaterial(_blurShader);
         _uiRenderPass = new UIRenderPass(blurMaterial, _layerMask);
+        Instance = this;
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
@@ -54,6 +59,67 @@ public class UIRendererFeature : ScriptableRendererFeature
 
     public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
     {
-        _uiRenderPass?.Setup(_execblur, _execGlassMorphism, _blurRate, _blurWidth, _blurRenderScale);
+        var execBlur = _parameter.ExecBlur;
+        var execGlassMorphism = _parameter.ExecGlassMorphism;
+        var blurRate = _parameter.BlurRate;
+#if UNITY_EDITOR
+        if (_debugForceChange)
+        {
+            execBlur = _debugExecBlur;
+            execGlassMorphism = _debugExecGlassMorphism;
+            blurRate = _debugBlurRate;
+        }
+#endif
+        _uiRenderPass?.Setup(execBlur, execGlassMorphism, blurRate, _blurWidth, _blurRenderScale);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            StopBlur();
+            StopGlassMorphism();
+            _uiRenderPass?.Dispose();
+            Instance = null;
+        }
+    }
+
+    public bool IsExecBlur()
+    {
+        return _parameter.ExecBlur;
+    }
+
+    public void ExecBlur(float rate = 1f)
+    {
+        _parameter.ExecBlur = true;
+        ChangeBlurRate(rate);
+    }
+
+    public void StopBlur(float rate = 0f)
+    {
+        _parameter.ExecBlur = false;
+        ChangeBlurRate(rate);
+    }
+
+    public bool IsExecGlassMorphism()
+    {
+        return _parameter.ExecGlassMorphism;
+    }
+
+    public void ExecGlassMorphism(float rate = 1f)
+    {
+        _parameter.ExecGlassMorphism = true;
+        ChangeBlurRate(rate);
+    }
+
+    public void StopGlassMorphism(float rate = 0f)
+    {
+        _parameter.ExecGlassMorphism = false;
+        ChangeBlurRate(rate);
+    }
+
+    public void ChangeBlurRate(float rate)
+    {
+        _parameter.BlurRate = rate;
     }
 }
